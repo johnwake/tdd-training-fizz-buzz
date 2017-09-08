fdda542c4265701d0286dbc0e8b56a8484840676
john681611 <john.harvey@marks-and-spencer.com>
2017-09-08 13:20:05 +0100

    fixing tests

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -1,18 +1,19 @@
-let expect    = require("chai").expect;
-let fizzBuzz = require("../src/fizzBuzz");
+let expect = require('chai').expect;
+let fizzBuzz = require('../src/fizzBuzz');
 
-describe("Fizz Buzz App", function() {
+describe('Fizz Buzz App', () => {
 
-    describe("Multiple of 3 returns Fizz", function() {
-        input = 3;
-        fizzBuzzclass = new fizzBuzz();
+    it('Multiple of 3 returns Fizz', () => {
+        let input = 3;
+        let fizzBuzzclass = new fizzBuzz();
 
-        expect(fizzBuzzclass.run(input)).to.equal("Fizz");
+        expect(fizzBuzzclass.run(input)).to.equal('Fizz');
     });
 
-    describe("Multiple of 5 returns Buzz", function() {});
+    it('Multiple of 5 returns Buzz', () => {
+    });
 
-    describe("Multiple of 3 and 5 returns FizzBuzz", function() {});
+    it('Multiple of 3 and 5 returns FizzBuzz', () => {});
 
-    describe("If not a Multiple of 3 or 5 the returns the number", function() {});
+    it('If not a Multiple of 3 or 5 the returns the number', () => {});
 });
