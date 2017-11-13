f0a9599c7c3846a2b655c5e7f49afc63c91ef208
john681611 <john.harvey@marks-and-spencer.com>
2017-11-13 11:47:08 +0000

    44 lines for all

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -2,18 +2,19 @@ let expect = require('chai').expect;
 let fizzBuzz = require('../src/fizzBuzz');
 
 describe('Fizz Buzz App', () => {
-
     it('Multiple of 3 returns Fizz', () => {
-        let input = 3;
-        let fizzBuzzclass = new fizzBuzz();
-
-        expect(fizzBuzzclass.run(input)).to.equal('Fizz');
+        expect(fizzBuzz(3)).to.equal('Fizz');
     });
 
     it('Multiple of 5 returns Buzz', () => {
+        expect(fizzBuzz(5)).to.equal('Buzz');
     });
 
-    it('Multiple of 3 and 5 returns FizzBuzz', () => {});
+    it('Multiple of 3 and 5 returns FizzBuzz', () => {
+        expect(fizzBuzz(15)).to.equal('FizzBuzz');
+    });
 
-    it('If not a Multiple of 3 or 5 the returns the number', () => {});
+    it('If not a Multiple of 3 or 5 the returns the number', () => {
+        expect(fizzBuzz(input)).to.equal(8);
+    });
 });
